f2079fa43afb0872e921a0d01af2ea0f1061365c
SOMEKATA Shiro <88067020+somekata@users.noreply.github.com>
2024-10-07 17:24:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 document.getElementById('learn-more').addEventListener('click', function() {
   alert('サイトは近日公開予定です。お楽しみに！');
 });
-// ページビューカウンターを保存するためにlocalStorageを使用
-if (localStorage.getItem('page_view_count')) {
-  // 既存のカウントを取得して増加させる
-  var count = parseInt(localStorage.getItem('page_view_count'));
-  count++;
-} else {
-  // 初回訪問の場合
-  var count = 1;
-}
 
-// 更新されたカウントをlocalStorageに保存
-localStorage.setItem('page_view_count', count);
-
-// カウントをHTMLに表示
-document.getElementById('counter').textContent = count;
